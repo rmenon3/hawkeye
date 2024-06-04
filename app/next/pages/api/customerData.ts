dbdc0c59f99f4a546ee9b3a416178f0d4cc59468
rmenon3 <rahul.menon@litmus7.com>
2024-06-05 03:30:42 +0530

    Initial Commit - Nextjs Project

--- app/next/pages/api/customerData.ts
+++ app/next/pages/api/customerData.ts
@@ -1,77 +1,98 @@
-// import cors from '../../middleware/cors';
-const handler = async (req: any, res: any) => {
-    try {
-        console.log("Inside Handler")
-        const {
-            query: {  domain },
-            method,
-          } = req;
-          console.log(domain, method);
-          let urlDomain = domain.replace(/^https?:\/\//i, "");
-
-// // import cors from "cors";
-
-          // Remove 'www.'
-          urlDomain = urlDomain.replace(/^www\./i, "");
-        //   cors(req, res, async () => {
-        //     // Your API logic here
-        //     const response = await fetch('http://data.similarweb.com/api/v1/data?domain='+ urlDomain);
-        //     const data = await response.json();
-        //     console.log("1", data);
-        //     res.status(200).json(data);
-        //   });
-        const response = await fetch('http://data.similarweb.com/api/v1/data?domain='+ urlDomain,{
-            mode: 'no-cors'
-          });
-        const data = await response.json();
-        console.log(JSON.stringify(data))
-        // const response = await fetch('http://data.similarweb.com/api/v1/data?domain='+ domain);
-        // const data = await response.json();
-        return res.end(JSON.stringify(data));
-    } catch (err:any) {
-        return res.end(JSON.stringify({'error':err.message}));
-    }
-
-}
-
-// const corsMiddleware = cors({
-//     origin: '*', // Allow requests from any origin (you can specify specific origins instead)
-//     methods: ['GET', 'POST'], // Allow only specific methods
-//     allowedHeaders: ['Content-Type'], // Allow only specific headers
-// });
-
-// export default function handler(req: any, res: any) {
-//     // Apply CORS middleware
-//     corsMiddleware(req, res, async () => {
-//         // Your API logic here
+// // import cors from '../../middleware/cors';
+// const handler = async (req: any, res: any) => {
+//     try {
+//         console.log("Inside Handler")
 //         const {
-//             query: { domain },
+//             query: {  domain },
 //             method,
-//         } = req;
-//         console.log(domain, method);
-//         const response = await fetch('http://data.similarweb.com/api/v1/data?domain=' + domain);
+//           } = req;
+//           console.log(domain, method);
+//           let urlDomain = domain.replace(/^https?:\/\//i, "");
+
+// // // import cors from "cors";
+
+//           // Remove 'www.'
+//           urlDomain = urlDomain.replace(/^www\./i, "");
+//         //   cors(req, res, async () => {
+//         //     // Your API logic here
+//         //     const response = await fetch('http://data.similarweb.com/api/v1/data?domain='+ urlDomain);
+//         //     const data = await response.json();
+//         //     console.log("1", data);
+//         //     res.status(200).json(data);
+//         //   });
+//         const response = await fetch('http://data.similarweb.com/api/v1/data?domain='+ urlDomain,{
+//             mode: 'no-cors'
+//           });
 //         const data = await response.json();
-//         console.log(JSON.stringify(data));
-//         res.status(200).json(data);
-//     });
+//         console.log(JSON.stringify(data))
+//         // const response = await fetch('http://data.similarweb.com/api/v1/data?domain='+ domain);
+//         // const data = await response.json();
+//         return res.end(JSON.stringify(data));
+//     } catch (err:any) {
+//         return res.end(JSON.stringify({'error':err.message}));
+//     }
+
 // }
 
-// import { NextRequest } from 'next/server'
-// import cors from '../../lib/cors'
+// // const corsMiddleware = cors({
+// //     origin: '*', // Allow requests from any origin (you can specify specific origins instead)
+// //     methods: ['GET', 'POST'], // Allow only specific methods
+// //     allowedHeaders: ['Content-Type'], // Allow only specific headers
+// // });
 
-// export const config = {
-//   runtime: 'edge',
-// }
+// // export default function handler(req: any, res: any) {
+// //     // Apply CORS middleware
+// //     corsMiddleware(req, res, async () => {
+// //         // Your API logic here
+// //         const {
+// //             query: { domain },
+// //             method,
+// //         } = req;
+// //         console.log(domain, method);
+// //         const response = await fetch('http://data.similarweb.com/api/v1/data?domain=' + domain);
+// //         const data = await response.json();
+// //         console.log(JSON.stringify(data));
+// //         res.status(200).json(data);
+// //     });
+// // }
 
-// export default async function handler(req: NextRequest) {
-//   // `cors` also takes care of handling OPTIONS requests
-//   return cors(
-//     req,
-//     new Response(JSON.stringify({ message: 'Hello World!' }), {
-//       status: 200,
-//       headers: { 'Content-Type': 'application/json' },
-//     })
-//   )
-// }
+// // import { NextRequest } from 'next/server'
+// // import cors from '../../lib/cors'
 
-export default handler;
+// // export const config = {
+// //   runtime: 'edge',
+// // }
+
+// // export default async function handler(req: NextRequest) {
+// //   // `cors` also takes care of handling OPTIONS requests
+// //   return cors(
+// //     req,
+// //     new Response(JSON.stringify({ message: 'Hello World!' }), {
+// //       status: 200,
+// //       headers: { 'Content-Type': 'application/json' },
+// //     })
+// //   )
+// // }
+
+// export default handler;
+
+export default function handler(req:any, res:any, next:any) {
+    async function getURI(url:any) {
+      try {
+        const response:any = await fetch('http://data.similarweb.com/api/v1/data?domain='+ url);;
+        const data = await response.json();
+        return res.end(JSON.stringify(data));
+        // if (response.status !== 200) {
+        //   return res.status(response.status).json({ type: 'error', message: response.statusText });
+        // } else {
+        //     return res.status(response.status).json(JSON.stringify(response.data));
+        // }
+      } catch (error:any) {
+        console.log(error.message, "ERR")
+        // return res.status(500).json({ type: 'error', message: error.message });
+      }
+    } 
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'application/json');
+    getURI(req.query.domain);
+  }
